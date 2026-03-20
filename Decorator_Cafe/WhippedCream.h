3eeeeb382711b3cc02bac83cec03f4901beeb9a8
una-07 <esirkepulpan8@gmail.com>
2026-03-20 14:26:20 +0500

    Create WhippedCream.h

---
+++ Decorator_Cafe/WhippedCream.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "BeverageDecorator.h"
+
+class WhippedCream : public BeverageDecorator {
+public:
+    WhippedCream(std::shared_ptr<Beverage> bev) : BeverageDecorator(bev) {}
+
+    std::string GetDescription() override {
+        return beverage->GetDescription() + " + Cream";
+    }
+
+    double Cost() override {
+        return beverage->Cost() + 0.7;
+    }
+};
